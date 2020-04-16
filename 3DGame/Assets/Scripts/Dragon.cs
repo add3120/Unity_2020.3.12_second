af3f78b9b5e17331c672c73dcdcc0b34723b2c56
add3120 <62015397+add3120@users.noreply.github.com>
2020-04-16 21:43:05 +0800

    學習生成、飛龍攻擊腳本

--- 3DGame/Assets/Scripts/Dragon.cs
+++ 3DGame/Assets/Scripts/Dragon.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Dragon : MonoBehaviour
 {
@@ -6,9 +7,26 @@ public class Dragon : MonoBehaviour
     public float speed = 300;
     [Header("虛擬搖桿")]
     public Joystick joy;
+    [Header("攻擊冷卻時間")]
+    public float cd = 1;
+    [Header("延遲生成火球時間")]
+    public float delayFire = 0.5f;
+    [Header("火球")]
+    public GameObject fireBall;
+    [Header("火球移動速度"), Range(1, 5000)]
+    public float speedFireBall = 300;
 
     // 第一種寫法 : 需要欄位
     //public Transform tra;
+
+    /// <summary>
+    /// 動畫控制器
+    /// </summary>
+    private Animator ani;
+    /// <summary>
+    /// 計時器
+    /// </summary>
+    private float timer;
 
     /// <summary>
     /// 移動
@@ -35,11 +53,50 @@ public class Dragon : MonoBehaviour
 
         Vector3 pos = transform.position;         // 取得飛龍座標
         pos.x = Mathf.Clamp(pos.x, 30, 70);       // 數學.夾住(值，最小，最大)
+        pos.z = Mathf.Clamp(pos.z, 20, 30);       
         transform.position = pos;                 // 飛龍座標 = 夾住座標
+    }
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack()
+    {
+        timer += Time.deltaTime;             // 計時器 遞增
+
+        if (timer >= cd)                     // 如果 計時器 >= 冷卻
+        {
+            timer = 0;                       // 計時器 歸零
+            ani.SetTrigger("攻擊觸發");      // 動畫控制器.設定處發器("參數名稱")
+
+            StartCoroutine(DelayFierBall());
+        }
+    }
+
+    private IEnumerator DelayFierBall()
+    {
+        yield return new WaitForSeconds(delayFire);
+
+        Vector3 posFire = transform.position;                 // 火球座標 = 飛龍座標
+        posFire.z += 4.2f;                                    // 微調 Z 軸
+        posFire.y += 2.2f;                                    // 微調 Y 軸
+
+        GameObject temp = Instantiate(fireBall, posFire, Quaternion.identity);  // 生成(物件，座標，角度)
+
+        // Quaternion.identity Unity 角度類型 - 零角度
+
+        temp.GetComponent<Rigidbody>().AddForce(0, 0, speedFireBall);
+    }
+
+    private void Start()
+    {
+        // 取得元件<泛型>()
+        ani = GetComponent<Animator>();
     }
 
     private void Update()
     {
         Move();
+        Attack();
     }
 }
