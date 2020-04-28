43d981a7d6bfd49f3fb8f62087fc8dfd7b7ea22b
add3120 <62015397+add3120@users.noreply.github.com>
2020-04-28 21:49:52 +0800

    未完成,錄影檔6開始做

--- 3DGame/Assets/Scripts/Monster.cs
+++ 3DGame/Assets/Scripts/Monster.cs
@@ -4,11 +4,19 @@ public class Monster : MonoBehaviour
 {
     [Header("怪物資料")]
     public MonsterData data;
+    [Header("補血藥水")]
+    public GameObject propHp;
+
+    // 補血藥水掉落機率 : 30% (0.3)
+    // Random.Range(0, 1) - 小於 0.3
+    // if (隨機 <= 0.3) 掉落補血藥水
 
     private Animator ani;
+    private float hp;
 
     private void Start()
     {
+        hp = data.hp;
         ani = GetComponent<Animator>();
     }
 
@@ -18,9 +26,9 @@ public class Monster : MonoBehaviour
     /// <param name="damage">接收到的傷害值</param>
     public void Damage(float damage)
     {
-        data.hp -= damage;
+        hp -= damage;
 
-        if (data.hp <= 0) Dead();
+        if (hp <= 0) Dead();
     }
 
     /// <summary>
@@ -29,5 +37,18 @@ public class Monster : MonoBehaviour
     private void Dead()
     {
         ani.SetBool("死亡開關", true);
+        DropProp();
+    }
+
+    /// <summary>
+    /// 掉落道具
+    /// </summary>
+    private void DropProp()
+    {
+        float rHP = Random.Range(0f, 1f);
+        print("掉落補血藥水的機率 : " + rHP);
+        if (rHP <= data.propHP) Instantiate(propHp, transform.position + Vector3.right * Random.Range(-1f, 1f), Quaternion.identity);
+        float rCD = Random.Range(0f, 1f);
+        if (rCD <= data.propCD) Instantiate(propCD, transform.position + Vector3.right * Random.Range(-1f, 1f), Quaternion.identity);
     }
 }
