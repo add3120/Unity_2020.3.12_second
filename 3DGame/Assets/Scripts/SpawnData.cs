96adf9277b3277632de0e6faac5662cc16cc4394
add3120 <62015397+add3120@users.noreply.github.com>
2020-05-01 16:37:50 +0800

    蝙蝠、生成怪物腳本

--- 3DGame/Assets/Scripts/SpawnData.cs
+++ 3DGame/Assets/Scripts/SpawnData.cs
@@ -3,10 +3,21 @@
 [CreateAssetMenu(fileName = "關卡資料", menuName = "HAO/關卡資料")]
 public class SpawnData : ScriptableObject
 {
-
+    public SpawnTime[] spawn;
 }
 
+// 序列化 : 顯示在Unity 屬性面板 (class 專用)
+[System.Serializable]
 public class SpawnTime
 {
+    public  string name = "時間";
+    public float time;
+    public SpawnMonster[] monsters;
+}
 
+[System.Serializable]
+public class SpawnMonster
+{
+    public GameObject monster;
+    public float x;
 }
